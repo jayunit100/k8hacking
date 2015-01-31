087a7c5fc40fcb5b427fe389cc4e85af10765d31
root <root@host18-rack11.scale.openstack.engineering.redhat.com>
2015-01-31 00:20:57 +0000

    syntax skeletaon, not useful yet

--- cnegus/kube-deploy.sh
+++ cnegus/kube-deploy.sh
@@ -1,2 +1,18 @@
+kubectl delete rc database
+kubectl delete rc web 
+
+kubectl delete -f webserver-rc.yaml
+kubectl delete -f db-rc.yaml
+
+for line in $(kubectl get pods | grep "dbc" | cut -d' ' -f 1 | grep 1); do kubectl delete pod $line ; done; 
+
+for line in $(kubectl get pods | grep "web" | cut -d' ' -f 1 | grep 1); do kubectl delete pod $line ; done; 
+
+for line in $(kubectl get pods | grep "data" | cut -d' ' -f 1 | grep 1); do kubectl delete pod $line ; done; 
+
+echo "done deleteing......."
+sleep 10
+echo "restart...."
+
 kubectl create -f webserver-rc.yaml
-kubectyl create -f db-rc.yaml
+kubectl create -f db-rc.yaml
