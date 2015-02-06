cd544a3bb46887c3db9419dd501b33a7bdd4b7bc
jay vyas <jayunit100@gmail.com>
2015-02-06 17:04:31 -0500

    Update gss_db.sql

--- cnegus/database/gss_db.sql
+++ cnegus/database/gss_db.sql
@@ -738,7 +738,7 @@ CREATE TABLE IF NOT EXISTS `general_log` (
   `event_time` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `user_host` mediumtext NOT NULL,
   `thread_id` int(11) NOT NULL,
-  `server_id` int(10) unsigned NOT NULL,
+  `server_id` int(10) unsigned NOT NULL,
   `command_type` varchar(64) NOT NULL,
   `argument` mediumtext NOT NULL
 ) ENGINE=CSV DEFAULT CHARSET=utf8 COMMENT='General log';
