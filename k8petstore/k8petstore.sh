9956a80d4e5c0d8ddc8a3b2142be5e6fcd18c199
root <root@host18-rack11.scale.openstack.engineering.redhat.com>
2015-03-11 14:54:52 +0000

    WIP

--- k8petstore/k8petstore.sh
+++ k8petstore/k8petstore.sh
@@ -1,14 +1,15 @@
 echo "WRITING KUBE FILES , will overwrite the jsons, then testing pods. is kube clean ready to go?"
 
-#PUBLIC_IP="10.1.4.89"
-PUBLIC_IP="192.168.100.200"
-FE="1"
-LG="1"
-SLAVE="0"
+PUBLIC_IP="10.1.4.89"
+PUBLIC_IP2="10.1.4.87"
+#PUBLIC_IP="192.168.100.200"
+FE="5"
+LG="5"
+SLAVE="5"
 
 cat << EOF > fe-rc.json
 {
-  "id": "frontend-controller",
+  "id": "fectrl",
   "kind": "ReplicationController",
   "apiVersion": "v1beta1",
   "desiredState": {
@@ -18,7 +19,7 @@ cat << EOF > fe-rc.json
       "desiredState": {
          "manifest": {
            "version": "v1beta1",
-           "id": "frontend-controller",
+           "id": "frontendCcontroller",
            "containers": [{
              "name": "frontend-go-restapi",
              "image": "jayunit100/k8petstore",
@@ -27,7 +28,7 @@ cat << EOF > fe-rc.json
        },
        "labels": {
          "name": "frontend",
-         "uses": "redisslave,redis-master"
+         "uses": "redis-master"
        }
       }},
   "labels": {"name": "frontend"}
@@ -36,7 +37,7 @@ EOF
 
 cat << EOF > bps-load-gen-rc.json
 {
-  "id": "bps-load-gen-controller",
+  "id": "bpsloadgenrc",
   "kind": "ReplicationController",
   "apiVersion": "v1beta1",
   "desiredState": {
@@ -46,7 +47,7 @@ cat << EOF > bps-load-gen-rc.json
       "desiredState": {
          "manifest": {
            "version": "v1beta1",
-           "id": "bps-load-gen-controller",
+           "id": "bpsLoadGenController",
            "containers": [{
              "name": "bps",
              "image": "jayunit100/bigpetstore-load-generator",
@@ -59,7 +60,7 @@ cat << EOF > bps-load-gen-rc.json
          "uses": "frontend"
         }
       }},
-  "labels": {"name": "bps-load-gen-controller"}
+  "labels": {"name": "bpsLoadGenController"}
 }
 EOF
 
@@ -70,7 +71,7 @@ cat << EOF > fe-s.json
   "apiVersion": "v1beta1",
   "port": 3000,
   "containerPort": 3000,
-  "publicIPs":["$PUBLIC_IP","10.1.4.89","127.0.0.1","10.1.4.82"],
+  "publicIPs":["$PUBLIC_IP","$PUBLIC_IP2", "10.1.4.89","127.0.0.1","10.1.4.82"],
   "selector": {
     "name": "frontend"
   },
@@ -82,17 +83,16 @@ EOF
 
 cat << EOF > rm.json
 {
-  "id": "redis-master",
+  "id": "redismaster",
   "kind": "Pod",
   "apiVersion": "v1beta1",
   "desiredState": {
     "manifest": {
       "version": "v1beta1",
-      "id": "redis-master",
+      "id": "redismaster",
       "containers": [{
         "name": "master",
         "image": "jayunit100/guestbook-redis-master",
-        "cpu": 0,
         "ports": [{
           "containerPort": 6379,
           "hostPort": 6379
@@ -108,7 +108,7 @@ EOF
 
 cat << EOF > rm-s.json
 {
-  "id": "redis-master",
+  "id": "redismaster",
   "kind": "Service",
   "apiVersion": "v1beta1",
   "port": 6379,
@@ -140,7 +140,7 @@ EOF
 
 cat << EOF > slave-rc.json
 {
-  "id": "redis-slave-controller",
+  "id": "redissc",
   "kind": "ReplicationController",
   "apiVersion": "v1beta1",
   "desiredState": {
@@ -150,7 +150,7 @@ cat << EOF > slave-rc.json
       "desiredState": {
          "manifest": {
            "version": "v1beta1",
-           "id": "redis-slave-controller",
+           "id": "redissc",
            "containers": [{
              "name": "slave",
              "image": "jayunit100/guestbook-redis-slave",
@@ -188,7 +188,7 @@ for i in `seq 1 150`;
 do
     ### Just testing that the front end comes up.  Not sure how to test total entries etc... (yet)
     echo "Trying curl ... $i . expect a few failures while pulling images... " 
-    curl "localhost:3000" > result
+    curl "$PUBLIC_IP:3000" > result
     cat result
     cat result | grep -q "k8-bps"
     if [ $? -eq 0 ]; then
