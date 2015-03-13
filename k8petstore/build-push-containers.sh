d2beba0cb59ee7e08427a927c406b63e05bd9aa9
jayunit100 <jayunit100@gmail.com>
2015-03-13 13:31:22 -0400

    cleanup containers

--- k8petstore/build-push-containers.sh
+++ k8petstore/build-push-containers.sh
@@ -1,11 +1,11 @@
 #K8PetStore version is tied to the redis version.  We will add more info to version tag later.
 version="r.2.8.19"
-docker build -t jayunit100/redis:$version ./redis/
-docker build -t jayunit100/redis-master:$version ./redis-master
-docker build -t jayunit100/redis-slave:$version ./redis-slave
-docker build -t jayunit100/k8petstore:$version ./redis-slave
+docker build -t jayunit100/k8-petstore-redis:$version ./redis/
+docker build -t jayunit100/k8-petstore-redis-master:$version ./redis-master
+docker build -t jayunit100/k8-petstore-redis-slave:$version ./redis-slave
+docker build -t jayunit100/k8-petstore-web-server:$version ./web-server
 
-docker push jayunit100/k8petstore:$version
-docker push jayunit100/redis:$version
-docker push jayunit100/redis-master:$version
-docker push jayunit100/redis-slave:$version
+docker push jayunit100/k8-petstore-redis:$version
+docker push jayunit100/k8-petstore-redis-master:$version
+docker push jayunit100/k8-petstore-redis-slave:$version
+docker push jayunit100/k8-petstore-web-server:$version
